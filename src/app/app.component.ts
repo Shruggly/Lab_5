6d62611de7877903dc899df11a34fa0efa49c046
Tommy <shruggly@gmail.com>
2023-03-03 12:31:33 +0000

    lab5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'data-binding-app';
+
+
+  buttonNum:number = 0;
+  hide:boolean = true;
+
+
+  onClick() {
+    this.buttonNum++;
+  }
+
+  onDblClick() {
+    if(this.hide == true) {
+      this.hide = false;
+    }else if (this.hide == false) {
+      this.hide = true;
+    }
+  }
+
 }
